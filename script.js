ab4a8dea0dd94a3908f70e9dcf6f02e73ed8cb84
Fabiola Odio <fabiolaodio777@hotmail.com>
2022-09-14 18:33:21 -0600

    Inicializando repositorio

---
+++ script.js
@@ -0,0 +1,17 @@
+const num7=document.getElementById('seven').value;
+const screen=document.getElementById('screen-p');
+
+
+num7.addEventListener('click', insertar);
+
+function insertar(){
+    screen.innerHTML=num7;
+}
+
+
+
+
+
+
+
+
